b6e0aa4ca6e1fb685b6767420dc0f15d0b64e996
dnyaneshwarh1718-afk <dnyaneshwarh1718@gmail.com>
2026-01-26 02:06:35 +0530

    main

--- main.py
+++ main.py
@@ -1,13 +1,15 @@
 from src.ingestion.load_csv_to_mysql import load_all_raw_csvs
 from src.pipeline.run_sql_etl import run_full_sql_pipeline
+from src.pipeline.train_best_pipeline import run_best_model_pipeline
 
-if __name__ == '__main__':
+if __name__ == "__main__":
     print("Loading RAW CSVs into MySQL server..")
     load_all_raw_csvs()
 
     print("Running SQL ETL Pipeline")
     run_full_sql_pipeline()
 
-    print("Done. Master table ready: loan_master")
+    print("Running Best Model Training Pipeline")
+    run_best_model_pipeline()
 
-    
+    print("Done ")
