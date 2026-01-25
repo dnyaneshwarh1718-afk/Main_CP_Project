aac81c125d7cf10593fd3f55f1210efae4950256
dnyaneshwarh1718-afk <dnyaneshwarh1718@gmail.com>
2026-01-25 15:18:41 +0530

    config

--- src/config/settings.py
+++ src/config/settings.py
@@ -2,7 +2,7 @@ from pathlib import Path
 
 PROJECT_ROOT = Path(__file__).resolve().parents[2]
 
-PROJECT_ROOT = PROJECT_DIR / "data"
+DATA_DIR = PROJECT_ROOT / "data"
 RAW_DATA_DIR = DATA_DIR / "raw"
 
 SQL_DIR = PROJECT_ROOT / "SQL"
