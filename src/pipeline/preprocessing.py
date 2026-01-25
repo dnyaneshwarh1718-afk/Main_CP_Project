88b6f010a4d3f9a91ed5040f026cc208fb8c1601
dnyaneshwarh1718-afk <dnyaneshwarh1718@gmail.com>
2026-01-25 21:55:36 +0530

    Data_preprocessing

---
+++ src/pipeline/preprocessing.py
@@ -0,0 +1,38 @@
+import pandas as pd
+from sklearn.compose import ColumnTransformer
+from sklearn.pipeline import Pipeline
+from sklearn.preprocessing import OneHotEncoder, StandardScaler
+from sklearn.impute import SimpleImputer
+
+def build_preprocessor(df: pd.DataFrame, target_col: str):
+    """
+    returns: preprocessor (ColumnTransformer)
+    feature_columns (list)
+    """
+
+    x = df.drop(columns = [target_col])
+
+    # split column type
+
+    num_cols = x.select_dtypes(include = ["int64", "float64"]).columns.tolist()
+    cat_cols = x.select_dtypes(include = ["object"]).columns.tolist()
+
+    # Numeric Pipeline
+    num_pipeline = Pipeline(steps=[
+        ("imputer", SimpleImputer(strategy="median")),
+        ("scaler", StandardScaler())
+    ])
+
+    cat_pipeline = Pipeline(steps=[
+        ("imputer", SimpleImputer(strategy="most_frequent")),
+        ("onehot", OneHotEncoder(handle_unknown="ignore"))
+    ])
+
+    preprocessor = ColumnTransformer(
+        transformers=[
+            ("num",num_pipeline, num_cols),
+            ("cat",cat_pipeline, cat_cols),
+        ]
+    )
+
+    return preprocessor, num_cols, cat_cols
