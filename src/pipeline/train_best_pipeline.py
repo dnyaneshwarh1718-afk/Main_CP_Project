8e57a9518d7f5b7894ebfbe1554e557c9c4783d4
dnyaneshwarh1718-afk <dnyaneshwarh1718@gmail.com>
2026-01-26 02:05:38 +0530

    final model traning pipeline

--- src/pipeline/train_best_pipeline.py
+++ src/pipeline/train_best_pipeline.py
@@ -4,17 +4,14 @@ from src.utils.logger import setup_logger
 
 logger = setup_logger("train_best_pipeline")
 
-def run_best_model_logger():
-    logger.info("Fetchingloan_master data")
+def run_best_model_pipeline():
+    logger.info("Fetching loan_master data...")
     df = fetch_master_table()
     logger.info(f"loan_master shape: {df.shape}")
 
-    logger.info("Training multiple models and selecting best")
-    best_name, best_path, comparison_path, results = train_all_models_and_select_best(df)
+    logger.info("Training multiple models and selecting best...")
+    best_name, best_path, metrics_path = train_all_models_and_select_best(df)
 
     logger.info(f"Best Model: {best_name}")
     logger.info(f"Best Model Saved: {best_path}")
-    logger.info(f"Comparison Saved: {comparison_path}")
-    logger.info(f"Results:{results}")
-
-    
+    logger.info(f"Metrics Saved: {metrics_path}")
