d416a6252907c71dce79a17e5e4d72884533fd20
dnyaneshwarh1718-afk <dnyaneshwarh1718@gmail.com>
2026-01-25 17:40:28 +0530

    Updated SQL pipeline runner

--- src/utils/sql_runner.py
+++ src/utils/sql_runner.py
@@ -1,25 +1,39 @@
+import sqlparse
 from src.db.mysql_connection import get_mysql_connection
 from src.utils.logger import setup_logger
 
 logger = setup_logger("sql_runner")
 
+
 def run_sql_file(sql_path):
     logger.info(f"Running: {sql_path.name}")
 
-    with open(sql_path, "r", encoding = "utf-8") as f:
-        script = f.read()
+    with open(sql_path, "r", encoding="utf-8") as f:
+        sql_script = f.read()
+
+    #  sqlparse safely splits SQL statements
+    statements = [s.strip() for s in sqlparse.split(sql_script) if s.strip()]
 
     conn = get_mysql_connection()
     cursor = conn.cursor()
 
-    statements = [s.strip() for s in script.split(";") if s.strip()]
+    try:
+        for stmt in statements:
+            cursor.execute(stmt)
 
-    for stmt in statements:
-        cursor.execute(stmt)
+             #  important for SELECT statements
+            if cursor.with_rows:
+                cursor.fetchall()
 
-    conn.commit()
-    cursor.close()
-    conn.close()
+        conn.commit()
+        logger.info(f" Completed: {sql_path.name}")
 
-    logger.info(f"Done: {sql_path.name}")
-    
+    except Exception as e:
+        conn.rollback()
+        logger.error(f" Failed: {sql_path.name}")
+        logger.error(str(e))
+        raise
+
+    finally:
+        cursor.close()
+        conn.close()
