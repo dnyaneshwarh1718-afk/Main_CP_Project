cbb66a829db6fa274c9fa2296d40d4044eaa74fe
dnyaneshwarh1718-afk <dnyaneshwarh1718@gmail.com>
2026-01-29 01:05:31 +0530

    updated

--- main.py
+++ main.py
@@ -2,6 +2,7 @@ from src.ingestion.load_csv_to_mysql import load_all_raw_csvs
 from src.pipeline.run_sql_etl import run_full_sql_pipeline
 from src.pipeline.train_binary_class_train_pipeline import run_best_model_pipeline
 from src.pipeline.train_multiclass_pipeline import run_multiclass_pipeline
+from src.inference.run_inference import run_inference
 
 if __name__ == "__main__":
     print("Loading RAW CSVs into MySQL server..")
@@ -16,4 +17,7 @@ if __name__ == "__main__":
     print("Running Multiclass Training Pipeline (A/B/C/D)")
     run_multiclass_pipeline()
 
-    print("Done ")
+    print("Running inference pipeline...")
+    run_inference()
+
+    print("Done. Scored data ready for Power BI.")
